ae8836b948f7f22bba9034cdea74c1c1b0755a5e
Cyrus Wong <cywong@vtc.edu.hk>
2018-11-12 03:59:10 -0500

    Fix killed process uplaod immediately.

--- Model/Screenshot.py
+++ Model/Screenshot.py
@@ -1,41 +1,50 @@
-import os
+import datetime
+import json
 import threading
-import pyautogui
-import requests
-
 from time import sleep
 
+import psutil
+import requests
 
-class Screenshot(threading.Thread):
+from Model.Event import ProcessEvent
+
+
+class ProcessMonitor(threading.Thread):
 
     def __init__(self, api: str, key: str):
         super().__init__()
-        self.name = "Screenshot"
+        self.name = "ProcessMonitor"
         self.api = api
         self.key = key
+        self.black_list_process = []
 
     def run(self):
+        count = 0
         while True:
-            try:
-                response = requests.get("https://" + self.api + "/screenshot",
-                                         headers={"x-api-key": self.key})
-                if response.ok:
-                    # Take screenshot
-                    pic = pyautogui.screenshot()
-                    # Save the image
-                    file_name = 'Screenshot.jpeg'
-                    pic.save(file_name)
-                    with open(file_name, "rb") as image_file:
-                        files = {'file': (file_name, image_file, 'image/jpeg', {'Expires': '0'})}
-                        upload = response.json()
-                        url = upload['url'].replace("s3.amazonaws.com","s3-accelerate.amazonaws.com")
-                        upload_screenshot = requests.post(url, data=upload['fields'], files=files)
-                        print('screen shot uploaded!')
-                    os.remove(file_name)
+            running_process = []
+            killed = False
+            for proc in psutil.process_iter(
+                    attrs=['pid', 'name', 'cpu_times', 'memory_percent', 'memory_info', 'io_counters']):
+                if proc.info['name'] in self.black_list_process:
+                    print("killed ", proc.info)
+                    proc.kill()
+                    ps = ProcessEvent(proc, datetime.datetime.now(), True)
+                    killed = True
                 else:
-                    print(response.status_code)
-                    print(response.reason)
+                    ps = ProcessEvent(proc, datetime.datetime.now(), False)
+                running_process.append(ps.event)
 
-            except Exception as e:
-                print(e)
-            sleep(55)
+            if killed or count <= 1 or count % 10 == 0:
+                try:
+                    data = json.dumps(running_process).encode('utf8')
+                    response = requests.post("https://" + self.api + "/process", data=data,
+                                             headers={"x-api-key": self.key})
+                    if response.ok:
+                        self.black_list_process = list(map(lambda x: x.strip(), response.json().split(",")))
+                    else:
+                        print(response.status_code)
+                        print(response.reason)
+                except Exception as e:
+                    print(e)
+            count = count + 1
+            sleep(5)
