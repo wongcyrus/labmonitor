e12ae1ba0bb7ab73d9cbd095116caec3ea69c02a
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2018-11-15 13:44:32 +0800

    Fix wrong override file!

--- Model/Screenshot.py
+++ Model/Screenshot.py
@@ -1,50 +1,41 @@
-import datetime
-import json
+import os
 import threading
-from time import sleep
-
-import psutil
+import pyautogui
 import requests
 
-from Model.Event import ProcessEvent
+from time import sleep
 
 
-class ProcessMonitor(threading.Thread):
+class Screenshot(threading.Thread):
 
     def __init__(self, api: str, key: str):
         super().__init__()
-        self.name = "ProcessMonitor"
+        self.name = "Screenshot"
         self.api = api
         self.key = key
-        self.black_list_process = []
 
     def run(self):
-        count = 0
         while True:
-            running_process = []
-            killed = False
-            for proc in psutil.process_iter(
-                    attrs=['pid', 'name', 'cpu_times', 'memory_percent', 'memory_info', 'io_counters']):
-                if proc.info['name'] in self.black_list_process:
-                    print("killed ", proc.info)
-                    proc.kill()
-                    ps = ProcessEvent(proc, datetime.datetime.now(), True)
-                    killed = True
+            try:
+                response = requests.get("https://" + self.api + "/screenshot",
+                                         headers={"x-api-key": self.key})
+                if response.ok:
+                    # Take screenshot
+                    pic = pyautogui.screenshot()
+                    # Save the image
+                    file_name = 'Screenshot.jpeg'
+                    pic.save(file_name)
+                    with open(file_name, "rb") as image_file:
+                        files = {'file': (file_name, image_file, 'image/jpeg', {'Expires': '0'})}
+                        upload = response.json()
+                        url = upload['url'].replace("s3.amazonaws.com","s3-accelerate.amazonaws.com")
+                        upload_screenshot = requests.post(url, data=upload['fields'], files=files)
+                        print('screen shot uploaded!')
+                    os.remove(file_name)
                 else:
-                    ps = ProcessEvent(proc, datetime.datetime.now(), False)
-                running_process.append(ps.event)
+                    print(response.status_code)
+                    print(response.reason)
 
-            if killed or count <= 1 or count % 10 == 0:
-                try:
-                    data = json.dumps(running_process).encode('utf8')
-                    response = requests.post("https://" + self.api + "/process", data=data,
-                                             headers={"x-api-key": self.key})
-                    if response.ok:
-                        self.black_list_process = list(map(lambda x: x.strip(), response.json().split(",")))
-                    else:
-                        print(response.status_code)
-                        print(response.reason)
-                except Exception as e:
-                    print(e)
-            count = count + 1
-            sleep(5)
+            except Exception as e:
+                print(e)
+            sleep(55)
