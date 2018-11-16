9a96d661d837846cee18418888a5cbc526906672
Cyrus <6193567+wongcyrus@users.noreply.github.com>
2018-11-16 10:47:43 +0800

    Logic to send killed process immediately.

--- Model/ProcessMonitor.py
+++ Model/ProcessMonitor.py
@@ -22,27 +22,29 @@ class ProcessMonitor(threading.Thread):
         count = 0
         while True:
             running_process = []
+            killed = False
             for proc in psutil.process_iter(
                     attrs=['pid', 'name', 'cpu_times', 'memory_percent', 'memory_info', 'io_counters']):
                 if proc.info['name'] in self.black_list_process:
                     print("killed ", proc.info)
                     proc.kill()
                     ps = ProcessEvent(proc, datetime.datetime.now(), True)
+                    killed = True
                 else:
                     ps = ProcessEvent(proc, datetime.datetime.now(), False)
                 running_process.append(ps.event)
-            count = count + 1
-            if count % 10 == 0:
+
+            if killed or count <= 1 or count % 10 == 0:
                 try:
                     data = json.dumps(running_process).encode('utf8')
                     response = requests.post("https://" + self.api + "/process", data=data,
                                              headers={"x-api-key": self.key})
                     if response.ok:
                         self.black_list_process = list(map(lambda x: x.strip(), response.json().split(",")))
-
                     else:
                         print(response.status_code)
                         print(response.reason)
                 except Exception as e:
                     print(e)
+            count = count + 1
             sleep(5)
